869979a4cfd7f2dba0b7be3f2c6ad8138299cbda
jiumi <jiumilove@163.com>
2018-10-13 22:39:15 +0800

    首页完成

---
+++ frontend/pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgSrc: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+    itemThumb:'https://tse4.mm.bing.net/th?id=OIP.Elyd51yliaem9DZaQjiMawHaE7&pid=Api',
+    itemSrc:'http://ouvyoji2r.bkt.clouddn.com/2018/10/3245483770.png',
+    itemGo:'http://ouvyoji2r.bkt.clouddn.com/2018/10/459207981.png'
+  },
+  onLoad: function () {
+    
+  },
+ 
+})
